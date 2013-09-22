653d42a84af27eaba0512d4a375cafd1b8797594
Regulus343 <codyjassman@gmail.com>
2013-09-22 03:10:41 -0600

    updated home page

--- src/views/core/home.blade.php
+++ src/views/core/home.blade.php
@@ -3,8 +3,8 @@
 @section(Config::get('fractal::section'))
 
 	<div class="jumbotron">
-		<h1>Welcome to FractalCMS 0.1a</h1>
-		<p>FractalCMS is currently being developed.</p>
+		<h1>Welcome to Fractal</h1>
+		<p>Fractal is currently in development and is at the PRE-ALPHA stage. Stay tuned.</p>
 	</div>
 
 @stop
