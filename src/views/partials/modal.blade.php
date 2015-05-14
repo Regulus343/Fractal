eae3a984a42ff74dfff65a9406bdd4c71756fe33
Cody Jassman <me@codyjassman.com>
2015-05-14 09:21:14 -0600

    added default text to modal title heading markup to make HTML valid

--- src/views/partials/modal.blade.php
+++ src/views/partials/modal.blade.php
@@ -3,7 +3,7 @@
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-				<h4 class="modal-title"></h4>
+				<h4 class="modal-title">Info</h4>
 			</div>
 
 			<div class="modal-body">
@@ -23,7 +23,7 @@
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-				<h4 class="modal-title"></h4>
+				<h4 class="modal-title">Info</h4>
 			</div>
 
 			<div class="modal-body">
